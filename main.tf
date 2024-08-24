91d4a6e060764c7314b1f1c0c52199234fcec044
akshay <abc@gmail.com>
2024-08-24 12:19:57 +0000

    added changes

---
+++ main.tf
@@ -0,0 +1,5 @@
+module "vpc" {
+
+ source = "./modules/VPC"
+ region = var.region
+}
---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.35.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+
+terraform {
+  backend "s3" {
+    bucket = "for-tf-use"
+    key    = "modules/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
