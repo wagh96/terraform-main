b3dfc567182238e2531732ecf7f0dd56ffdc0753
wagh96 <115559496+wagh96@users.noreply.github.com>
2024-08-27 11:25:24 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 module "vpc" {
-  source = "git::https://$(git_pat)@github.com/wagh96/terraform-modules.git//modules/VPC"
+  source = "git::ssh://git@github.com/wagh96/terraform-modules.git//modules/VPC"
   cidr_block = var.cidr_block
 }
